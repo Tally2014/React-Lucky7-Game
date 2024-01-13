8ae70a78f35d6f7b61f1e8e72b6ec7c797a35d12
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-13 12:31:17 +0200

    isWinner Logic

--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -1,12 +1,13 @@
 import Dice from "./Dice"
 import {useState} from 'react';
-import { getRolls } from "./utils";
+import { getRolls, sum } from "./utils";
 
 export default function LuckyN({numDice=2, goal=7}) {
     const [dice, setDice] = useState(getRolls(numDice));
+    const isWinner = sum(dice) === goal;
   return (
     <main className="LuckyN">
-        <h1>Lucky{goal}</h1>
+        <h1>Lucky{goal} {isWinner && "- You Win!"}</h1>
         <Dice dice={dice}/>
     </main>
   )
