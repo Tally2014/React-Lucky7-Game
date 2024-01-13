1edaed2343bb18aebe9dad63949fc3b14732cff0
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-13 12:36:34 +0200

    Add button to re-roll the dice

--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -5,10 +5,14 @@ import { getRolls, sum } from "./utils";
 export default function LuckyN({numDice=2, goal=7}) {
     const [dice, setDice] = useState(getRolls(numDice));
     const isWinner = sum(dice) === goal;
+    const roll = () =>{
+        return setDice(getRolls(numDice));
+    }
   return (
     <main className="LuckyN">
         <h1>Lucky{goal} {isWinner && "- You Win!"}</h1>
         <Dice dice={dice}/>
+        <button onClick={roll}>Re-Roll Dice</button>
     </main>
   )
 }
