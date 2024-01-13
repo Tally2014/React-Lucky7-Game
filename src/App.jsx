3169ef375ba3621e39719a35dc6ccf688b958b02
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-13 12:00:28 +0200

    Add Dynamic color styling to Die and Dice Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      <Dice dice={[3,5,2]}/>
+      <Dice dice={[3,5,2]} color={'blue'}/>
+      <Dice dice={[2,4]}/>
+      <Dice dice={[1,3,6,2,5,4]} color={'green'}/>
     </>
   )
 }
--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,10 +1,10 @@
 import './Dice.css'
 import Die from './Die'
 
-export default function Dice({dice}) {
+export default function Dice({dice, color="chocolate"}) {
   return (
     <section className='Dice'>
-        {dice.map((v,i) => <Die key={i} val={v}/>)}
+        {dice.map((v,i) => <Die key={i} val={v} color={color}/>)}
     </section>
   )
 }
