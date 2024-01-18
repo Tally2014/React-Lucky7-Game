412b624924cf982f720975a785f5a656c702ddf5
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-01-18 22:35:42 +0200

    655. Passing Functions As Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import LuckyN from './LuckyN'
+import { sum } from './utils';
+
+function lessThan4(dice){
+  return sum(dice) < 4;
+}
+
+function allSameValue(dice){
+  return dice.every((v) => v === dice[0]);
+}
+
 
 function App() {
 
   return (
     <>
-      <LuckyN/>
+      <LuckyN winCheck={lessThan4}/>
+      <LuckyN winCheck={allSameValue}/>
     </>
   )
 }
--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -1,16 +1,16 @@
 import Dice from "./Dice"
 import {useState} from 'react';
-import { getRolls, sum } from "./utils";
+import { getRolls } from "./utils";
 
-export default function LuckyN({numDice=2, goal=7}) {
+export default function LuckyN({numDice=2, winCheck}) {
     const [dice, setDice] = useState(getRolls(numDice));
-    const isWinner = sum(dice) === goal;
+    const isWinner = winCheck(dice);
     const roll = () =>{
         return setDice(getRolls(numDice));
     }
   return (
     <main className="LuckyN">
-        <h1>Lucky{goal} {isWinner && "- You Win!"}</h1>
+        <h1>Lucky {isWinner && "- You Win!"}</h1>
         <Dice dice={dice}/>
         <button onClick={roll}>Re-Roll Dice</button>
     </main>
